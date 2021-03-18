7ae8f7e44df9f179282259ffa76ae446d18afa95
primo9293 <42128291+primo9293@users.noreply.github.com>
2021-03-18 11:41:30 -0500

    Init Commit - Estructura calendario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+      },
+    ]
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  PagesRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
